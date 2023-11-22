146401434934d99ecbef90a701d8cb0c8c08d1fc
Cemputus <cemputus019@gmail.com>
2023-11-22 20:40:59 +0300

    PIZZAS

--- src/components/Blog/Bdata.jsx
+++ src/components/Blog/Bdata.jsx
@@ -4,84 +4,83 @@ const Bdata = [
       imgsrc: "/images/blog-img-1.jpg",
       pname: "Cheezy Pizza Toppins",
       price:
-        "Mauris fermentum dictum magna. Sed laoreet ali­quam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit.",
+      "Turning Ingredients into Art, One Dish at a Time."
     },
     {
       id: 2,
       imgsrc: "/images/blog-img-2.jpg",
       pname: "Cheezy Pizza Toppins",
       price:
-        "Mauris fermentum dictum magna. Sed laoreet ali­quam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit.",
+      "Where Passion Meets the Palate, Culinary Magic Happens."
     },
     {
       id: 3,
       imgsrc: "/images/blog-img-3.jpg",
       pname: "Cheezy Pizza Toppins",
       price:
-        "Mauris fermentum dictum magna. Sed laoreet ali­quam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit.",
+      "In the Kitchen, I Dance with Flavors and Tantalize Taste Buds."
     },
     {
       id: 4,
       imgsrc: "/images/blog-img-4.jpg",
       pname: "Cheezy Pizza Toppins",
       price:
-        "Mauris fermentum dictum magna. Sed laoreet ali­quam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit.",
+      "Savor the Moment, Taste the Passion."
     },
     {
       id: 5,
       imgsrc: "/images/blog-img-5.jpg",
       pname: "Cheezy Pizza Toppins",
       price:
-        "Mauris fermentum dictum magna. Sed laoreet ali­quam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit.",
+      "Whisking Dreams into Reality, Culinary Excellence Unleashed."
     },
     {
       id: 6,
       imgsrc: "/images/blog-img-6.jpg",
       pname: "Cheezy Pizza Toppins",
       price:
-        "Mauris fermentum dictum magna. Sed laoreet ali­quam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit.",
-    },
+      "Crafting Culinary Masterpieces, One Plate at a Time."    },
     {
       id: 7,
       imgsrc: "/images/blog-img-7.jpg",
       pname: "Cheezy Pizza Toppins",
       price:
-        "Mauris fermentum dictum magna. Sed laoreet ali­quam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit.",
+      "Turning Ingredients into Art, One Dish at a Time."
     },
     {
       id: 8,
       imgsrc: "/images/blog-img-8.jpg",
       pname: "Cheezy Pizza Toppins",
       price:
-        "Mauris fermentum dictum magna. Sed laoreet ali­quam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit.",
+      "Infusing Passion into Every Recipe, One Bite at a Time."
     },
     {
       id: 9,
       imgsrc: "/images/blog-img-9.jpg",
       pname: "Cheezy Pizza Toppins",
       price:
-        "Mauris fermentum dictum magna. Sed laoreet ali­quam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit.",
+      "Bite into Bliss, Courtesy of Culinary Creativity."
     },
     {
       id: 10,
       imgsrc: "/images/blog-img-10.jpg",
       pname: "Cheezy Pizza Toppins",
       price:
-        "Mauris fermentum dictum magna. Sed laoreet ali­quam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit.",
+      "Where Precision Meets Palate Pleasure."
     },
     {
       id: 11,
       imgsrc: "/images/blog-img-11.jpg",
       pname: "Cheezy Pizza Toppins",
       price:
-        "Mauris fermentum dictum magna. Sed laoreet ali­quam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit.",
+      "In the Kitchen, Every Spice Tells a Tale."
     },
     {
       id: 12,
       imgsrc: "/images/blog-img-12.jpg",
       pname: "Cheezy Pizza Toppins",
       price:
-        "Mauris fermentum dictum magna. Sed laoreet ali­quam leo. Ut tellus dolor, dapibus eget, elementum vel, cursus eleifend, elit.",
+      "Bite into Bliss, Courtesy of Culinary Creativity."
     },
   ];
   
--- src/components/Blog/Blogpost.jsx
+++ src/components/Blog/Blogpost.jsx
@@ -80,3 +80,172 @@ const Wrapper = styled.section`
 `;
 
 export default Blogpost;
+
+
+// import React from "react";
+// import styled from "styled-components";
+// import { FiArrowRight } from "react-icons/fi";
+
+// const Blogpost = (props) => {
+//   return (
+//     <Wrapper>
+//       <div className="product-card">
+//         <div className="thumbnail">
+//           <img src={props.imgsrc} alt="" />
+//         </div>
+//         <div className="prod-details">
+//           <span className="name">{props.pname}</span>
+//           <span className="price">{props.price}</span>
+//         </div>
+//       </div>
+//       <a className="read-btn">
+//         Read More <FiArrowRight />
+//       </a>
+//     </Wrapper>
+//   );
+// };
+
+// const Wrapper = styled.section`
+//   .product-card {
+//     width: 32rem;
+//     margin-bottom: 1rem;
+//     .thumbnail {
+//       width: 100%;
+//       background-color: rgba(0, 0, 0, 0.05);
+//       margin-bottom: 10px;
+//       padding: 10px;
+//       display: flex;
+//       align-items: center;
+//       border-radius: 1rem;
+//       img {
+//         transition: all ease 0.3s;
+//         display: block;
+//         width: 100%;
+//       }
+//     }
+//     .prod-details {
+//       .name {
+//         font-size: 2rem;
+//         margin: 1.6rem 0 1rem;
+//         display: block;
+//         text-overflow: ellipsis;
+//         white-space: nowrap;
+//         overflow: hidden;
+//         color: white; /* Set the text color to white */
+//       }
+//       .price {
+//         font-size: 1.6rem;
+//         color: white; /* Set the text color to white */
+//         font-weight: 400;
+//         line-height: 1.5;
+//       }
+//     }
+//     &:hover {
+//       .thumbnail {
+//         img {
+//           transform: scale(1.1);
+//         }
+//       }
+//     }
+//   }
+//   .read-btn {
+//     color: white; /* Set the text color to white */
+//     display: flex;
+//     align-items: center;
+//     gap: 0.5rem;
+//     font-size: 1.5rem;
+//     cursor: pointer;
+//   }
+//   @media only screen and (min-width: ${({ theme }) => theme.media.tab}) {
+//     .product-card {
+//       width: 28rem;
+//     }
+//   }
+// `;
+
+// export default Blogpost;
+
+
+
+// import React from "react";
+// import styled from "styled-components";
+// import { FiArrowRight } from "react-icons/fi";
+
+// const Blogpost = (props) => {
+//   return (
+//     <Wrapper>
+//       <div className="product-card">
+//         <div className="thumbnail">
+//           <img src={props.imgsrc} alt="" />
+//         </div>
+//         <div className="prod-details">
+//           <span className="name">{props.pname}</span>
+//           <span className="price">{props.price}</span>
+//         </div>
+//       </div>
+//       <a className="read-btn">
+//         Read More <FiArrowRight />
+//       </a>
+//     </Wrapper>
+//   );
+// };
+
+// const Wrapper = styled.section`
+//   .product-card {
+//     width: 32rem;
+//     margin-bottom: 1rem;
+//     .thumbnail {
+//       width: 100%;
+//       background-color: rgba(0, 0, 0, 0.05);
+//       margin-bottom: 10px;
+//       padding: 10px;
+//       display: flex;
+//       align-items: center;
+//       border-radius: 1rem;
+//       img {
+//         transition: all ease 0.3s;
+//         display: block;
+//         width: 100%;
+//       }
+//     }
+//     .prod-details {
+//       .name {
+//         font-size: 2rem;
+//         margin: 1.6rem 0 1rem;
+//         display: block;
+//         text-overflow: ellipsis;
+//         white-space: nowrap;
+//         overflow: hidden;
+//         color: cream; /* Set the text color to cream */
+//       }
+//       .price {
+//         font-size: 1.6rem;
+//         color: white; /* Set the text color to white */
+//         font-weight: 400;
+//         line-height: 1.5;
+//       }
+//     }
+//     &:hover {
+//       .thumbnail {
+//         img {
+//           transform: scale(1.1);
+//         }
+//       }
+//     }
+//   }
+//   .read-btn {
+//     color: ${({ theme }) => theme.colors.bg};
+//     display: flex;
+//     align-items: center;
+//     gap: 0.5rem;
+//     font-size: 1.5rem;
+//     cursor: pointer;
+//   }
+//   @media only screen and (min-width: ${({ theme }) => theme.media.tab}) {
+//     .product-card {
+//       width: 28rem;
+//     }
+//   }
+// `;
+
+// export default Blogpost;
